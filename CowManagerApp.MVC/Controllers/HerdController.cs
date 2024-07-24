469b159e67a8d689e2ed92af7d34893011330b3f
FilipWspanialy <filipwspanialy@gmail.com>
2024-07-24 10:01:02 +0200

    herd - nie działa

---
+++ CowManagerApp.MVC/Controllers/HerdController.cs
@@ -0,0 +1,44 @@
+﻿using CowManagerApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+
+namespace CowManagerApp.MVC.Controllers
+{
+    
+    public class HerdController : Controller
+    {
+        private readonly CowManagerContext _context;
+
+        public HerdController(CowManagerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if(_context.Herds == null)
+            {
+                return Problem("Entity set 'ApiContext.Movie'  is null.");
+            }
+
+            var herds = _context.Herds;
+
+            return View(await herds.ToListAsync());
+        }
+
+        public async Task<IActionResult> HerdDetails(int id)
+        {
+            var herd = await _context.Herds
+                .Include(s => s.Cows)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (herd == null)
+            {
+                return NotFound();
+            }
+
+            return View(herd);
+        }
+       
+    }
+}
