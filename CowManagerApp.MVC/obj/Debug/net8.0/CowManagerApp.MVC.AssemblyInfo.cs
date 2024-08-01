7b500b0e2f915487dce262c7034f1e0f95f3dd33
FilipWspanialy <filipwspanialy@gmail.com>
2024-08-01 13:03:26 +0200

    działa

--- CowManagerApp.MVC/obj/Debug/net8.0/CowManagerApp.MVC.AssemblyInfo.cs
+++ CowManagerApp.MVC/obj/Debug/net8.0/CowManagerApp.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CowManagerApp.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3159dedc2f0913af34fc94eb01671f98d99a94b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54078e8880fa5c91cd5649861b713b1a14c96f9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CowManagerApp.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CowManagerApp.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
